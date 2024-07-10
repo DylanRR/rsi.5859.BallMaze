b81fdeae3d86b1013ae2f25c285c717c1fe9fb38
DylanRR <dragaishis@gmail.com>
2024-07-10 09:55:57 -0500

    force sh to stay open after running

--- setup.sh
+++ setup.sh
@@ -29,4 +29,9 @@ echo "Setup completed successfully."
 
 # Pause and wait for the user to press Enter to close the terminal
 echo "Press Enter to close this window..."
-read
+while true; do
+    read -r -p "Press Enter to close this window..." input
+    if [ -z "$input" ]; then
+        break
+    fi
+done
