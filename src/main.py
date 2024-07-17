09c14e22476207534e59433ca4b6951b1152159d
DylanRR <Dylan@ravenswoodstudio.com>
2024-07-17 16:01:45 -0500

    refactor: Update main.py with ISR locking flags and fine calibration for track home and end positions

--- src/main.py
+++ src/main.py
@@ -40,6 +40,7 @@ def set_motor_direction(clockwise):
 # Generic Halt function
 def halt(message):
 	#ISR Locking Flags
+	global ISR_LOCK
 	ISR_LOCK = True
 	print(f"\n{message}")
 	toggle_motor(False)
@@ -74,6 +75,7 @@ trackEndFineCalibration = False
 
 def right_ls():
 	#ISR Locking Flags
+	global ISR_LOCK, ISR_SOFT_LOCK
 	if ISR_LOCK:
 		return
 	ISR_LOCK = True
@@ -83,7 +85,7 @@ def right_ls():
 
 	if not trackHomeCalibrated:
 		trackPosition = 0
-		move_motor(steps=50, clockwise=False)
+		move_motor(steps=100, clockwise=False)
 		trackHomeCalibrated = True
 	elif not trackHomeFineCalibration:
 		move_motor(steps=20, clockwise=False)
@@ -97,6 +99,7 @@ def right_ls():
 
 def left_ls():
 	#ISR Locking Flags
+	global ISR_LOCK, ISR_SOFT_LOCK
 	if ISR_LOCK:
 		return
 	ISR_LOCK = True
@@ -104,7 +107,7 @@ def left_ls():
 
 	global trackEndCalibrated, trackEnd, trackPosition, trackEndFineCalibration
 	if not trackEndCalibrated:
-		move_motor(steps=50, clockwise=True, delay=0.001)
+		move_motor(steps=100, clockwise=True, delay=0.001)
 		trackEndCalibrated = True
 	elif not trackEndFineCalibration:
 		move_motor(steps=20, clockwise=True, delay=0.001)
@@ -117,33 +120,35 @@ def left_ls():
 		
 
 def calibrateTrack():
+	#ISR Locking Flags
+	global ISR_SOFT_LOCK
 	ISR_SOFT_LOCK = True
 	print("Calibrating Track...")
 	global trackHomeCalibrated, trackEndCalibrated, trackPosition, trackSteps, trackHomeFineCalibration, trackEndFineCalibration
 	set_motor_direction(True)
 	while not trackHomeCalibrated:
 		mStep.on()
-		time.sleep(0.0001)
+		time.sleep(0.001)
 		mStep.off()
-		time.sleep(0.0001)
+		time.sleep(0.001)
 	while not trackHomeFineCalibration:
 		mStep.on()
-		time.sleep(0.01)
+		time.sleep(0.1)
 		mStep.off()
-		time.sleep(0.01)
+		time.sleep(0.1)
 
 	set_motor_direction(False)
 	while not trackEndCalibrated:
 		mStep.on()
-		time.sleep(0.0001)
+		time.sleep(0.001)
 		mStep.off()
-		time.sleep(0.0001)
+		time.sleep(0.001)
 		trackPosition += 1
 	while not trackEndFineCalibration:
 		mStep.on()
-		time.sleep(0.01)
+		time.sleep(0.1)
 		mStep.off()
-		time.sleep(0.01)
+		time.sleep(0.1)
 		trackPosition += 1
 	
 	trackSteps = trackEnd - trackHome
